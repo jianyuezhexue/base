8b0103032eb78a5713e789e53179bfd2230128c2
490513975@qq.com <490513975@qq.com>
2025-05-28 20:42:35 +0800

    ci: 同上

--- base.go
+++ base.go
@@ -59,13 +59,13 @@ type BaseModelInterface[T any] interface {
 // 公共模型属性
 type BaseModel[T any] struct {
 	Id                  uint64            `json:"id" uri:"id" search:"-" gorm:"primarykey"` // 主键
-	CreatedAt           db.LocalTime      `json:"createdAt" search:"-"`                     // 创建时间
-	UpdatedAt           db.LocalTime      `json:"updatedAt" search:"-"`                     // 更新时间
-	DeletedAt           gorm.DeletedAt    `json:"-" gorm:"index" search:"-"`                // 删除标记
 	CreateBy            string            `json:"createBy" gorm:"<-:create" search:"-"`     // 创建人
 	CreateByName        string            `json:"createByName" gorm:"<-:create" search:"-"` // 创建人名称
+	CreatedAt           db.LocalTime      `json:"createdAt" gorm:"<-:create"  search:"-"`   // 创建时间
 	UpdateBy            string            `json:"updateBy" search:"-"`                      // 更新人
 	UpdateByName        string            `json:"updateByName" search:"-"`                  // 更新人名称
+	UpdatedAt           db.LocalTime      `json:"updatedAt" search:"-"`                     // 更新时间
+	DeletedAt           gorm.DeletedAt    `json:"-" gorm:"index" search:"-"`                // 删除标记
 	Db                  *gorm.DB          `json:"-" gorm:"-" search:"-"`                    // 数据库连接
 	Ctx                 *gin.Context      `json:"-" gorm:"-" search:"-"`                    // 上下文
 	Preloads            map[string][]any  `json:"-" gorm:"-" search:"-"`                    // 预加载
